4300e9aace2738cb5bc4b0e2d11529101a0a1045
reshetnikd <dmitry.reshetnik@icloud.com>
2020-09-15 15:07:17 +0300

    Add CustomTextField

--- NavBarWithTitle/ContentView.swift
+++ NavBarWithTitle/ContentView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     let users = (1...100).map { number in "User \(number)" }
-    @State private var text = ""
-
+    
     var body: some View {
         NavigationView {
             GeometryReader { geometry in
@@ -32,19 +31,67 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct UserView: View {
+    @State private var text = ""
     @State var user: String
+    @State var showingTextField = false
     
     var body: some View {
         NavigationView {
             Text(self.user)
-        }.navigationBarItems(trailing:
-            HStack {
-                TextField("\(user)", text: self.$user)
-                    .font(.headline)
-                    .lineLimit(1)
+        }
+//        .navigationBarTitle(Text(self.user), displayMode: .inline)
+        .navigationBarItems(leading: HStack {
+            Button(action: {
+                self.showingTextField.toggle()
+            }, label: {
+                Image("pencil")
+                    .resizable()
                     .foregroundColor(.black)
-            }
-            .padding(.trailing, 140)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30, height: 20, alignment: .center)
+            })
+        }, trailing: CustomTextField(text: $user, isFirstResponder: showingTextField)
+            .frame(width: 175, height: 50)
+            .background(Color.clear)
         )
+    }
+}
+
+struct CustomTextField: UIViewRepresentable {
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+
+        @Binding var text: String
+        var didBecomeFirstResponder = false
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            text = textField.text ?? ""
+        }
+
+    }
+
+    @Binding var text: String
+    var isFirstResponder: Bool = false
+
+    func makeUIView(context: UIViewRepresentableContext<CustomTextField>) -> UITextField {
+        let textField = UITextField(frame: .zero)
+        textField.delegate = context.coordinator
+        return textField
+    }
+
+    func makeCoordinator() -> CustomTextField.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func updateUIView(_ uiView: UITextField, context: UIViewRepresentableContext<CustomTextField>) {
+        uiView.text = text
+        if isFirstResponder && !context.coordinator.didBecomeFirstResponder  {
+            uiView.becomeFirstResponder()
+            context.coordinator.didBecomeFirstResponder = true
+        }
     }
 }
